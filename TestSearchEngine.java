6067e688451298cecf3037b097eabae701e01490
epapanikolaou <elenipapanikolaou1996@gmail.com>
2016-11-24 00:35:36 +0200

    Add files via upload

---
+++ TestSearchEngine.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TestSearchEngine {
+	public static void main(String[] args) {
+		Scanner input = new SCanner(System.in);
+
+		try {
+			//connection with given database
+
+
+			System.out.println("Enter search words:");
+			String searchWord = input.next();
+
+			//sql looking for proper results
+
+			while(input.next()) {
+				System.out.println(+ input.getString("//database"));
+			}
+		}
+		catch(Exception e) {
+			System.out.println("Error!");
+		}
+	}
+}
