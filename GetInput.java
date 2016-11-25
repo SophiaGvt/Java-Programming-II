1f62d12dcfd625c4b5211181c0513d234ce6da82
nnikol <nnikol@dmst.aueb.gr>
2016-11-25 21:35:15 +0200

    Add files via upload
    Class with method that gets String input from user.
     Assigns input's value to variable.
     And also contains get method for that variable.

---
+++ GetInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * Class with method that gets String input from user.
+ * Assigns input's value to variable.
+ * And also contains get method for that variable.
+ */
+public class GetInput {
+	private Scanner sNew = new Scanner(System.in);
+    private String input;
+
+		public void promptTerm() {
+			System.out.println("Enter search term: ");
+        	input = sNew.nextLine();
+    	}
+		public String getTerm() {
+    	    return input;
+		}
+}
