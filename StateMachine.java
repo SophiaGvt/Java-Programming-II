71c90dc05834358fea00e6f0a70e17fbd865b1bd
Sophia Gavioti <SophiaGvt@users.noreply.github.com>
2016-11-29 11:57:08 +0200

    Update StateMachine.java

--- StateMachine.java
+++ StateMachine.java
@@ -6,9 +6,12 @@ import java.util.*;
 public class StateMachine {
 	try {
 		FileInputStream a = new FileInputStream("");
-		InputStreamReader filehtml = new  InputStreamReader(null, "");
+		InputStreamReader filehtml = new  InputStreamReader(a, "");
 	} 
-	catch{}
+	catch (FileNotFoundException e) {
+		System.err.println("Unable to open file ");
+		System.exit(1);
+	}
 	public void inTag() {
 		do {
 			int p = filehtml.read();
