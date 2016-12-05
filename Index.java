78a7a0ec885f8dedf4a68243a3ae5b0766020e8f
agmitsa <agmitsa@gmail.com>
2016-12-05 14:22:50 +0200

    δημιουργια ενος hashmap
    κλειδι το string και να δειχνει το url του αρχειου που διαβαζει

---
+++ Index.java
@@ -0,0 +1,9 @@
+import java.net.URL;
+import java.util.Hashtable; 
+import java.util.ArrayList;
+
+public class Index {
+Hashtable <String, ArrayList <URL>> ht1 = new Hashtable <String, ArrayList <URL>>();
+Hashtable<ArrayList <URL>, ArrayList <int>> ht2 = new Hashtable<ArrayList <URL>, ArrayList <int>>();
+
+}
