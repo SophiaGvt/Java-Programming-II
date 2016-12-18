c89866358b00ad6d145244ddecb16d57f9cda975
agmitsa <agmitsa@gmail.com>
2016-12-19 00:30:50 +0200

    ανανεωμενη κλαση index
    προσθεσα 3 περιπτωσεις
    1η: υπαρχει η λεξη ηδη στο ιδιο url οποτε απλα αλλαζω τις φορες που εμφανιζεται
    2η: υπαρχει η λεξη αλλα σε διαφορετικο url οποτε προσθετω ενα νεο posting στο υπαρχον arraylist
    3η: η λεξη εμφανιζεται πρωτη φορα οποτε την βαζω στο map

--- Index.java
+++ Index.java
@@ -15,32 +15,48 @@ public class Index {
 	
 	
 	@SuppressWarnings("deprecation")
-	public void createIndex (String filehtml, String url) throws MalformedURLException {
+	public void createIndex (String url) throws MalformedURLException {
 		 myMap = new Hashtable <String, ArrayList<Posting<String, Integer>>>();
 				
 		
 		Posting<String, Integer> posting;		
 		posting = new Posting<String, Integer>(url,0);
+		
+		ArrayList<Posting<String, Integer>> myArrayList;
+		myArrayList = new ArrayList<Posting<String, Integer>>();
 
 		/**
 		 * tokens from split method in here
 		 */
 		
 		
-		for(String token : Tokens.tokens()){//θελει τον πινακα απο τη split
+		for(String token : Tokens.tokens()){//o pinakas poy prokyptei apo th split
 			
 			if (myMap.containsKey(token)) {
 				
-				posting.setI(posting.getI()+1);
 				
-			} else {				
+				for(Posting<String, Integer> aPosting : myMap.get(token)) {
+					//edw elegxw thn periptwsh h leksh na yparxei mesa sto idio url
+					if(aPosting.getUrl().equals(url)) {
+						aPosting.setI(posting.getI()+1);
+					} else {//edw einai h periptwsh poy yparxei h leksh alla se diaforetiko url
+						
+						posting.setI(1);
+						myMap.get(token).add(posting);
+						break;//edw afou evale to posting den xreiazetai 
+						//na synexisei to loup opote break  
+					}
+				}
+				
+				
+			} else {//edw einai h periptwsh poy den yparxei h leksh kai apothikeuetai 1h fora				
 				posting.setI(1);
 				
+				myArrayList.add(posting);
+				myMap.put(token, myArrayList);
 			}
-			ArrayList<Posting<String, Integer>> myArrayList;
-			myArrayList = new ArrayList<Posting<String, Integer>>();
-			myArrayList.add(posting);
-			myMap.put(token, myArrayList);
+			
+			
 			
 		}
 		
