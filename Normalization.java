ad3fa102691579bbbb796b4ea820325754dfdab8
nnikol <nnikol@dmst.aueb.gr>
2016-12-25 18:55:46 +0200

    Κλάση για κανονικοποίηση input και html text.
    Συνδυασμός των κλάσεων InputNormalization και RemoveInputDuplicates. 
    Update1 : 
    Αφαίρεση ορίσματος από τη μέθοδο inputNormalization, προσθήκη δημιουργίας αντικειμένου της κλασης GetInput για να πάρουμε το input του χρήστη για τη μέθοδο inputNormalization, διόρθωση εμφάνισης Ελληνικών χαρακτήρων.

--- Normalization.java
+++ Normalization.java
@@ -10,18 +10,22 @@ import java.util.Set;
  * Class with methods to normalize user input text and text taken from html files.
  * @author Nikos
  */
-
 public class Normalization {
-
+  
+  //Create new GetInput object
+  GetInput gi = new GetInput();
+  //Call method to get inputText for inputNormalization() 
+  String inputText = gi.getInputTerm();
+  
   /**
    * Normalizes user input text.
    * @param inputText : user input text
    * @return String[] inWords (no duplicate values)
    */
-  public String[] inputNormalization(String inputText) {
+  public String[] inputNormalization() {
     inputText = Normalizer.normalize(inputText, Normalizer.Form.NFD)
                 .toLowerCase().replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
-    List<String> mylist = Arrays.asList(inputText.split("[^a-zá-ù0-9]+"));
+    List<String> mylist = Arrays.asList(inputText.split("[^a-zÎ±-Ï‰0-9]+"));
     Set<String> myhs = new HashSet<String>(mylist);
     String[] inWords = myhs.toArray(new String[myhs.size()]);
     return inWords;
@@ -35,7 +39,7 @@ public class Normalization {
   public String[] textNormalization(String htmlText) {
     htmlText = Normalizer.normalize(htmlText, Normalizer.Form.NFD)
                .toLowerCase().replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
-    String[] wordList = htmlText.split("[^a-zá-ù0-9]+");
+    String[] wordList = htmlText.split("[^a-zÎ±-Ï‰0-9]+");
     return wordList;
   }
 }
