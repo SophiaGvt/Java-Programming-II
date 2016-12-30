4373ef8088fb8dbd5fe69afa741e2adfd579df05
agmitsa <agmitsa@gmail.com>
2016-12-30 17:05:17 +0200

    Add files via upload

--- Index.java
+++ Index.java
@@ -2,56 +2,57 @@
  * @author amitsa
  */
 
-import java.awt.List;
-import java.io.File;
+
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Map;
 import java.util.Hashtable; 
 import java.util.ArrayList;
 
 public class Index {
-	Map <String, ArrayList<Posting<String, Integer>>> myMap;
+	static Map <String, ArrayList<Posting<String, Integer>>> myMap;
 	
 	
-	@SuppressWarnings("deprecation")
-	public void createIndex (String url) throws MalformedURLException {
+	
+	public void createIndex (String url, String[] tokens) throws MalformedURLException {
 		 myMap = new Hashtable <String, ArrayList<Posting<String, Integer>>>();
 				
 		
+		 
 		Posting<String, Integer> posting;		
-		posting = new Posting<String, Integer>(url,0);
+		
+		Posting<String, Integer> myPosting;
 		
 		ArrayList<Posting<String, Integer>> myArrayList;
-		myArrayList = new ArrayList<Posting<String, Integer>>();
+		
 
 		/**
 		 * tokens from split method in here
 		 */
 		
+		//String example = "At the age of 19, Michael told Andrew Ridgeley and close friends that he was bisexual. Michael also told one of his two sisters, but he was advised by friends not to tell his parents about his sexuality.";
+		//String ex1 = "AGE, age, Age Michael";
 		
-		for(String token : Tokens.tokens()){//o pinakas poy prokyptei apo th split
+		for(String token : tokens){//o pinakas poy prokyptei apo th split
 			
 			if (myMap.containsKey(token)) {
-				
-				
-				for(Posting<String, Integer> aPosting : myMap.get(token)) {
+												
 					//edw elegxw thn periptwsh h leksh na yparxei mesa sto idio url
-					if(aPosting.getUrl().equals(url)) {
-						aPosting.setI(posting.getI()+1);
+					myPosting = myMap.get(token).get(myMap.get(token).size()-1);
+					if(myPosting.getUrl().equals(url)) {
+						myPosting.setI(myPosting.getI()+1);
 					} else {//edw einai h periptwsh poy yparxei h leksh alla se diaforetiko url
-						
+						posting = new Posting<String, Integer>(url,0);
 						posting.setI(1);
 						myMap.get(token).add(posting);
-						break;//edw afou evale to posting den xreiazetai 
-						//na synexisei to loup opote break  
+						 
 					}
-				}
-				
-				
+												
 			} else {//edw einai h periptwsh poy den yparxei h leksh kai apothikeuetai 1h fora				
-				posting.setI(1);
 				
+				posting = new Posting<String, Integer>(url,0);
+				posting.setI(1);				
+				
+				myArrayList = new ArrayList<Posting<String, Integer>>();
 				myArrayList.add(posting);
 				myMap.put(token, myArrayList);
 			}
@@ -60,9 +61,10 @@ public class Index {
 			
 		}
 		
-		Sorting.writeIndexToFile(myMap);
+		//Sorting.sortedMap(myMap);
 		
-				
+		
+						
 	}
 
 }
