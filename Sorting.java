6bbcd629d109b8403afab7c73f0ff4b02db95fae
agmitsa <agmitsa@gmail.com>
2017-01-03 22:12:05 +0200

    Add files via upload
    κλαση sorting(περιλαμβανει μονο την sortedmap για ταξινομηση των λεξεων του map)

--- Sorting.java
+++ Sorting.java
@@ -1,9 +1,6 @@
 //ταξινομηση πινακων
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Formatter;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,7 +9,7 @@ import java.util.TreeSet;
 
 public class Sorting {
 	
-	private static Formatter output;
+	//private static Formatter output;
 	static TreeSet<String> sortedKeys;
 	public static void sortedMap(
 			Map<String, ArrayList<Posting<String, Integer>>> myMap) {
@@ -21,39 +18,7 @@ public class Sorting {
 		sortedKeys = new TreeSet<>(keys); 
 		
 	}
-	
-	public static void writeIndexToFile(
-			Map<String, ArrayList<Posting<String, Integer>>> myMap) {
-		
-		String path = "C:\\Users\\amitsa\\Desktop\\java_programming_2\\SearchEngine\\myIndex";
-		File f = new File(path);
-		
-		try {
-			output = new Formatter(f);
-		}
-		catch (FileNotFoundException e) {
-			System.err.println("Unable to open file: " 
-				+ e.getMessage());
 
-		}
-		catch (SecurityException securityException) {
-			
-			System.err.println("Write permission denied. Terminating. ");
-			System.exit(1);
-		}
-		sortedMap(myMap);
-		ArrayList<Posting<String, Integer>> myPostingList;
-		for (String key : sortedKeys) {
-			
-			output.format("%s|", key);
-			
-			myPostingList = myMap.get(key);
-			
-				for(Posting<String,Integer> myPosting : myPostingList) {
-				output.format("%s, %d; ",  myPosting.getUrl(), myPosting.getI() );
-				}
-		}
-	}
 
 	
 }
