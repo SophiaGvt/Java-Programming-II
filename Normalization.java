5e8899e3a8b248fc4e7f213a8916508b8687b915
nnikol <nnikol@dmst.aueb.gr>
2017-01-10 19:26:06 +0200

    Update Normalization.java

--- Normalization.java
+++ Normalization.java
@@ -1,49 +1,58 @@
+package javengers;
+
 import java.text.Normalizer;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 /**
- * Class with methods to normalize user input text and text taken from HTML
- * files, it also removes stop words from the latter.
+ * Class with methods to normalize user input text and text taken from HTML files, 
+ * it also removes stop words from the latter. 
  * 
  * @author Nikos
  */
 public class Normalization {
+    
+  /**
+   * Method that normalizes user input text.
+   * 
+   * @param inputText : user input text to normalize
+   * @return inputText : normalized user input text
+   */
+  public static String inputNormalization(String inputText) {
+    try {
+      // Seperate accent marks from text
+      inputText = Normalizer.normalize(inputText, Normalizer.Form.NFD);
+      // Remove the seperated accent marks
+      inputText = inputText.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+      // Lower case 
+      inputText = inputText.toLowerCase();
+      // Remove all non alphanumerical characters
+      inputText = inputText.replaceAll("[^a-zα-ω0-9]+", "");
+      return inputText;
+    } catch (Exception ex) {
+      return null;
+    }
+  }
 
-	/**
-	 * Normalizes user input text.
-	 * 
-	 * @param inputText
-	 *            : user input text
-	 * @return String[] inWords (no duplicate values)
-	 */
-	public static String[] inputNormalization() {
-		String inputText = HashtoArray.getInputTerm();
-		inputText = Normalizer.normalize(inputText, Normalizer.Form.NFD);
-		inputText = inputText.toLowerCase();
-		inputText = inputText.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
-		List<String> mylist = Arrays.asList(inputText.split("[^a-zα-ω0-9]+"));
-		Set<String> myhs = new HashSet<String>(mylist);
-		String[] inWords = myhs.toArray(new String[myhs.size()]);
-		return inWords;
-	}
-
-	/**
-	 * Normalizes text taken from HTML files.
-	 * 
-	 * @param htmlText
-	 *            : text from HTML
-	 * @return String[] wordList (contains duplicate values, does not contain
-	 *         stopwords)
-	 */
-	public static String[] textNormalization(String htmlText) {
-		htmlText = Normalizer.normalize(htmlText, Normalizer.Form.NFD);
-		htmlText = htmlText.toLowerCase();
-		htmlText = htmlText.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
-		String[] wordList = htmlText.split("[^a-zα-ω0-9]+");
-		String[] nwl = StopWordsRemoval.removeStopWords(wordList);
-		return nwl;
-	}
+  /**
+   * Method that normalizes text taken from HTML files and removes stop words from it.
+   * 
+   * @param htmlText : text from HTML to normalize
+   * @return : String[] wordList (contains duplicate values, does not contain stop words)
+   */
+  public static String[] textNormalization(String htmlText) {
+    try {
+      // Seperate accent marks from text
+      htmlText = Normalizer.normalize(htmlText, Normalizer.Form.NFD);
+      // Remove the seperated accent marks
+      htmlText = htmlText.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+      // Lower case
+      htmlText = htmlText.toLowerCase();
+      // split using all non alphanumerical characters as delimiters
+      String[] wordList = htmlText.split("[^a-zα-ω0-9]+");
+      // Remove stop words from wordList
+      String[] nwl = StopWordsRemoval.removeStopWords(wordList);
+      return nwl;
+    } catch (Exception ex) {
+      return null;
+    }
+  }
 }
